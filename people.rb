3d4ab0da9fcfc7645e88c469e6fa994f4b443be0
Cody House <codyahouse@gmail.com>
2014-10-23 13:53:41 -0400

    Completed exercise, ran program, success

--- people.rb
+++ people.rb
@@ -31,3 +31,10 @@ instructor1 = Instructor.new("Chris")
 puts instructor1.greeting
 student1 = Student.new("Cristina")
 puts student1.greeting
+
+puts instructor1.teach
+puts student1.learn
+
+#The methods below do not work because the methods are defined to the classes and they are inheriting only from the person class.
+#puts student1.teach
+#puts instructor1.learn
